2d5deff18a41c76ba36e22a75556b75a2419fb03
linh nguyen <ntthuylinh0397@gmail.com>
2020-05-13 09:48:03 +0700

    f commit

---
+++ MyAppLibrary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAppLibrary
+//
+//  Created by LinhNguyen on 5/13/20.
+//  Copyright © 2020 LinhNguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyLibrary/MyLibrary.swift
@@ -0,0 +1,21 @@
+//
+//  MyLibrary.swift
+//  MyLibrary
+//
+//  Created by LinhNguyen on 5/13/20.
+//  Copyright © 2020 LinhNguyen. All rights reserved.
+//
+
+public class MyLibrary {
+
+    let name = "MyFramework"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
